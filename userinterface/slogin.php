e7348bdde74f69135440b2f4aa10c5cc13057c16
adegokeolujide <adegokeolujidemic@gmail.com>
2023-12-17 22:22:05 +0100

    First commit, user interface designed

---
+++ userinterface/slogin.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="/SIMS/css/boostrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/SIMS/css/mycss.css" rel="stylesheet">
+
+
+    <title>Login</title>
+</head>
+
+<body>
+
+
+    <div class="mainDiv">
+        <div class="innerDiv">
+
+            <div class="container">
+                <div class="row" style="margin-top: 2%;">
+                    <div class="col-md-2">
+
+                    </div>
+                    <div class="col-md-8">
+                        <label class="text-info">
+                            <h4>Roll No.</h4>
+                        </label>
+                    </div>
+                    <div class="col-md-2">
+
+                    </div>
+                </div>
+                <div class="row" style="margin-top: 2%;">
+                    <div class="col-md-2">
+
+                    </div>
+                    <div class="col-md-8">
+                        <input class="loginrow" type="text">
+                    </div>
+                    <div class="col-md-2">
+
+                    </div>
+                </div>
+                <div class="row" style="margin-top: 2%;">
+                    <div class="col-md-2">
+
+                    </div>
+                    <div class="col-md-8">
+                        <label class="text-info">
+                            <h4>Password</h4>
+                        </label>
+                    </div>
+                    <div class="col-md-2">
+
+                    </div>
+                </div>
+                <div class="row" style="margin-top: 2%;">
+                    <div class="col-md-2">
+
+                    </div>
+                    <div class="col-md-8">
+                        <input class="loginrow" type="password">
+                    </div>
+                    <div class="col-md-2">
+
+                    </div>
+                </div>
+                <div class="row" style="margin-top: 2%;">
+                    <div class="col-md-2">
+
+                    </div>
+                    <div class="col-md-8">
+                        <div style="width:100%; display:flex; justify-content:end">
+                            <button class="btn btn-success">LOGIN</button>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="/SIMS/css/boostrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
